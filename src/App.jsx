97b26df734b392d569727cfbc361aedf5d535cf7
jaykumar2526 <jaykumar2526@gmail.com>
2024-10-23 20:28:17 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden hero">
     <div className="my-3 p-3">
       <h2 className="display-5">Bharat Times</h2>
-      <p className="lead fw-bold">This is the clock that shows the time in harat at all times.</p>
+      <p className="lead fw-bold">This is the clock that shows the time in Bharat at all times.</p>
     </div>
     <div className="bg-dark shadow-sm mx-auto main-div" style={{width:" 80%", height: "300px", borderRadius: "21px 21px 0 0"}}>
       <CurrrTime></CurrrTime>
